35a8be1d191ea01cb696011a5d8ae71d191bf6b6
iorn <wmt.tkn.121@gmail.com>
2022-09-08 16:59:39 +0900

    abc249 D

--- contest/abc249/c/main.py
+++ contest/abc249/c/main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def print_func_info(func):
     def wrapper(*args, **kwargs):
         print(f"executing {func}")
@@ -8,3 +11,26 @@ def print_func_info(func):
             print(f":return: {result}")
         return results
     return wrapper
+
+
+N, K = map(int, input().split())
+S = [list(input()) for _ in range(N)]
+ans = 0
+for i in range(1 << N):
+    k = 0
+    d = defaultdict(int)
+    for j in range(N):
+        if (i >> j) & 1:
+            k += 1
+            for s in S[j]:
+                d[s] += 1
+            # print(j)
+    # print(d)
+    cnt = 0
+    for x in d.values():
+        # print(x)
+        if x == K:
+            cnt += 1
+    ans = max(ans, cnt)
+
+print(ans)
--- contest/abc249/d/main.py
+++ contest/abc249/d/main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def print_func_info(func):
     def wrapper(*args, **kwargs):
         print(f"executing {func}")
@@ -8,3 +11,22 @@ def print_func_info(func):
             print(f":return: {result}")
         return results
     return wrapper
+
+
+MAX = 2*10**5
+
+c = [0]*(MAX+1)
+d = defaultdict(int)
+N = int(input())
+A = list(map(int, input().split()))
+ans = 0
+for a in A:
+    c[a] += 1
+
+for j in range(1, MAX+1):
+    k = 1
+    while j*k <= MAX:
+        i = j*k
+        ans += c[i]*c[j]*c[k]
+        k += 1
+print(ans)
