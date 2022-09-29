010bc77684764c4c89c4a14a8ec275934d43965e
iorn <wmt.tkn.121@gmail.com>
2022-09-29 13:45:28 +0900

    arc148 B

--- contest/arc148/b/main.py
+++ contest/arc148/b/main.py
@@ -8,3 +8,28 @@ def print_func_info(func):
             print(f":return: {result}")
         return results
     return wrapper
+
+
+def f(s: str):
+    convert = {"p": "d", "d": "p"}
+    for i in range(len(s)):
+        s[i] = convert[s[i]]
+    return s[::-1]
+
+
+N = int(input())
+S = list(input())
+# print(f(S))
+l = -1
+
+for i, s in enumerate(S):
+    if l == -1 and s == "p":
+        l = i
+    if l != -1 and s == "d":
+        break
+ans = "".join(S)
+for r in range(l, N):
+    ans = min(ans, "".join(S[:l]+f(S[l:r+1])+S[r+1:]))
+
+# print(S[:l], f(S[l:r+1]), S[r+1:])
+print("".join(ans))
