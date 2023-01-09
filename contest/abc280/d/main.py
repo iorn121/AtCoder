145f8aa0c420dbbe4694091e6765a52239adfd11
iorn121 <wmt.tkn.121@gmail.com>
2023-01-09 15:12:16 +0900

    abc280 D,E, abc 281 E

--- contest/abc280/d/main.py
+++ contest/abc280/d/main.py
@@ -18,3 +18,76 @@ def LS(): return list(sys.stdin.readline().rstrip().split())
 # 重複なし組み合わせ ans=list(itertools.combinations(L,2))
 # 重複あり組み合わせ ans=list(itertools.combinations_with_replacement(L,2))
 # nCr ans=math.comb(n,r)
+
+
+# Nまでの数を素数かどうか判定（エラトステネスのふるい）
+def prime_list(n: int):
+    is_prime = [True] * (n + 1)
+    is_prime[0], is_prime[1] = False, False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if is_prime[i]:
+            for j in range(2 * i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
+
+
+def prime_factorization(N: int):
+    arr = {}
+    sqrt = int(math.sqrt(N))
+    for i in range(2, sqrt+1):
+        if N % i == 0:
+            cnt = 0
+            while N % i == 0:
+                cnt += 1
+                N //= i
+            arr[i] = cnt
+    if N != 1:
+        arr[N] = 1
+    return arr
+
+
+# 1文字ずつ文字列をずらしたときの最長共通接頭辞の長さ
+def z_algorithm(S: string):
+    n = len(S)
+    rtn = [-1]*n
+    rtn[0] = n
+    i, j = 1, 0
+    while i < len(S):
+        while i+j < len(S) and S[j] == S[i+j]:
+            j += 1
+        rtn[i] = j
+        if j == 0:
+            i += 1
+        k = 1
+        while i+k < len(S) and k+rtn[k] < j:
+            rtn[i+k] = rtn[k]
+            k += 1
+        i += k
+        j -= k
+    return rtn
+
+
+# Nまでの積をxで何回割り切れるか
+def divide_num(N: int, x: int):
+    rtn = 0
+    i = 1
+    while pow(x, i) <= N:
+        rtn += N//pow(x, i)
+        i += 1
+    return rtn
+
+
+K = I()
+prime = prime_factorization(K)
+ans = 0
+for (k, v) in prime.items():
+    left, right = 0, k*v+1
+    while left+1 < right:
+        mid = (left+right)//2
+        x = divide_num(mid, k)
+        if x >= v:
+            right = mid
+        else:
+            left = mid
+    ans = max(ans, right)
+print(ans)
--- contest/abc281/e/main.py
+++ contest/abc281/e/main.py
@@ -1,3 +1,4 @@
+from array import array
 import bisect
 import collections
 import copy
@@ -11,10 +12,33 @@ def I(): return int(sys.stdin.readline().rstrip())
 def MI(): return map(int, sys.stdin.readline().rstrip().split())
 def LI(): return list(map(int, sys.stdin.readline().rstrip().split()))
 def S(): return sys.stdin.readline().rstrip()
+def SS(H): return [S() for _ in range(H)]
 def LS(): return list(sys.stdin.readline().rstrip().split())
+def ARRAY(L): return array("i", L)
 
 # 累積和 ans=list(itertools.accumulate(L))
 # 順列 ans=list(itertools.permutation(L))
 # 重複なし組み合わせ ans=list(itertools.combinations(L,2))
 # 重複あり組み合わせ ans=list(itertools.combinations_with_replacement(L,2))
 # nCr ans=math.comb(n,r)
+
+
+N, M, K = MI()
+A = LI()
+
+LR = ARRAY(sorted(A[:M:]))
+ans = [sum(LR[:K])]
+for i in range(N-M):
+    now = ans[-1]
+    if A[i] <= LR[K-1]:
+        now -= A[i]
+        if M > K:
+            now += LR[K]
+    LR.pop(bisect.bisect_left(LR, A[i]))
+    bisect.insort_left(LR, A[i+M])
+    if A[i+M] <= LR[K-1]:
+        now += A[i+M]
+        if M > K:
+            now -= LR[K]
+    ans.append(now)
+print(*ans)
