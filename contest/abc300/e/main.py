03d88ca34388a5368f36fcfeb3c677b4ba6af5a4
iorn121 <wmt.tkn.121@gmail.com>
2023-05-21 21:40:46 +0900

    abc300 E

--- contest/abc300/e/main.py
+++ contest/abc300/e/main.py
@@ -2,6 +2,7 @@ from typing import Generic, Iterable, Iterator, TypeVar, Optional, List
 from array import array
 from bisect import bisect_left, bisect_right, insort
 import collections
+from functools import lru_cache
 import copy
 import heapq
 import itertools
@@ -935,8 +936,90 @@ def make_divisors(n):
     return lower_divisors + upper_divisors[::-1]
 
 
+class ModInt:
+    def __init__(self, x, mod=998244353):
+        self.x = x % mod
+        self.mod = mod
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x*other.x) if isinstance(other, ModInt) else
+            ModInt(self.x*other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x*pow(other.x, self.mod-2, self.mod)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x*pow(other, self.mod-2, self.mod))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, self.mod)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, self.mod))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x-self.x) if isinstance(other, ModInt) else
+            ModInt(other-self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, self.mod-2, self.mod)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, self.mod-2, self.mod))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, self.mod)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, self.mod))
+        )
+
+
 def main():
     N = I()
+
+    inv = pow(5, -1, MOD)
+
+    @lru_cache(maxsize=None)
+    def f(v):
+        if v == 1:
+            return 1
+        res = 0
+        for i in range(2, 6+1):
+            if v % i:
+                continue
+            res += f(v // i)
+        return res * inv % MOD
+
+    print(f(N))
 
 
 if __name__ == "__main__":
