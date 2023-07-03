56798a5614844887a86d7aa0ab274a440b3f1225
iorn121 <wmt.tkn.121@gmail.com>
2023-07-03 23:53:02 +0900

    abc307 C

--- contest/abc307/c/main.py
+++ contest/abc307/c/main.py
@@ -1336,7 +1336,21 @@ class mf_graph:
 
 
 def main():
-    N = I()
+    def I():
+        h, w = map(int, input().split())
+        s = 0
+        for _ in range(h):
+            bitshift = 10
+            s = (s << bitshift) + int(input().replace("#", "1").replace(".", "0"), 2)
+        return s // (s & -s)
+
+    a, b, x = I(), I(), I()
+    for i in range(100):
+        if a | (b << i) == x or (a << i) | b == x:
+            print("Yes")
+            break
+    else:
+        print("No")
 
 
 if __name__ == "__main__":
