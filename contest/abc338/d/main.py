b3958c041f82202e265c927d011c7f8dac974ff9
iorn <wmt.tkn.121@gmail.com>
2024-01-31 11:34:38 +0900

    abc338 D

--- contest/abc338/d/main.py
+++ contest/abc338/d/main.py
@@ -1,19 +1,33 @@
-N, M = map(int, input().split())
-X = [int(x) - 1 for x in input().split()]
-L = [0] * (2 * N + 1)
-for s, t in zip(X, X[1:]):
-    if s > t:
-        t += N
-    d = t - s
-    L[s] += N - d
-    L[t] -= N - d
-    L[t] += d
-    L[s + N] -= d
-# print(L)
-for i in range(1, 2 * N + 1):
-    L[i] += L[i-1]
-# print(L)
-ans = 10**18
-for i in range(N):
-    ans = min(ans,L[i]+L[i+N])
-print(ans)
+from atcoder.lazysegtree import LazySegTree
+N,M=[int(x) for x in input().split()]
+X=[int(x)-1 for x in input().split()]
+mincost=sum(min((X[i]-X[i-1])%N,(X[i-1]-X[i])%N) for i in range(1,M))
+def op(x,y):
+  return x+y
+
+e=0
+
+def mapping(x,y):
+  return x+y
+
+def composition(x,y):
+  return x+y
+
+id_=0
+
+lst=LazySegTree(op, e, mapping, composition, id_, N)
+for i in range(1,M):
+  l,r=min(X[i],X[i-1]),max(X[i],X[i-1])
+  diff=max((r-l)%N,(l-r)%N)-min((r-l)%N,(l-r)%N)
+  if diff==0:
+    continue
+  if r-l<N-r+l:
+    lst.apply(l,r,diff)
+  else:
+    lst.apply(0,l,diff)
+    lst.apply(r,N,diff)
+
+ans=lst.get(0)
+for i in range(1,N):
+  ans=min(ans,lst.get(i))
+print(mincost+ans)
