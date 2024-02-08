f57e2b956d615cbef3e201f09fc5069f4aab6fc2
iorn <wmt.tkn.121@gmail.com>
2024-02-08 18:34:03 +0900

    abc238 E

--- contest/abc238/e/main.py
+++ contest/abc238/e/main.py
@@ -1,8 +1,8 @@
+import numpy as np
+from atcoder.dsu import DSU
 N, Q = map(int, input().split())
-temoti = []*N
+uf = DSU(N+1)
 for i in range(Q):
     l, r = map(int, input().split())
-    l -= 1
-    r -= 1
-    temoti[l].append(r)
-now = 0
+    uf.merge(l-1, r)
+print("Yes" if uf.same(0,N) else "No")
