52d64715ecad50a9a5f5d6e71c5d4ebad5ff6830
iorn121 <wmt.tkn.121@gmail.com>
2024-02-12 23:44:04 +0900

    abc232 E

--- contest/abc232/e/main.py
+++ contest/abc232/e/main.py
@@ -1,30 +1,25 @@
-from functools import reduce
-from operator import mul
 H, W, K = map(int, input().split())
 x1, y1, x2, y2 = map(int, input().split())
 
 
 MOD = 998244353
-W %= MOD
-H %= MOD
-ans = 0
-x = 1
-
-for tate in range(K+1):
-    yoko = K-tate
-    x *= yoko
-    x //= tate+1
-    if tate == 0:
-        if y1 != y2:
-            continue
-        else:
-            ans += (pow(W-1, yoko-1) % MOD)
-    elif yoko == 0:
-        if x1 != x2:
-            continue
-        else:
-            ans += (pow(H-1, tate-1) % MOD)
-    else:
-        ans += (pow(W-1, yoko-1) % MOD)*(pow(H-1, tate-1) % MOD)*x
-    ans %= MOD
-print(ans)
+dp=[[0]*4 for _ in range(K+1)]
+cur=2*(x1==x2)+(y1==y2)
+dp[0][cur]=1
+for i in range(1,K+1):
+    dp[i][0]+=dp[i-1][0]*(H+W-4)%MOD
+    dp[i][0]+=dp[i-1][1]*(W-1)%MOD
+    dp[i][0]+=dp[i-1][2]*(H-1)%MOD
+    dp[i][0]%=MOD
+    dp[i][1]+=dp[i-1][0]
+    dp[i][1]+=dp[i-1][1]*(H-2)%MOD
+    dp[i][1]+=dp[i-1][3]*(H-1)%MOD
+    dp[i][1]%=MOD
+    dp[i][2]+=dp[i-1][0]
+    dp[i][2]+=dp[i-1][2]*(W-2)%MOD
+    dp[i][2]+=dp[i-1][3]*(W-1)%MOD
+    dp[i][2]%=MOD
+    dp[i][3]+=dp[i-1][1]
+    dp[i][3]+=dp[i-1][2]
+    dp[i][3]%=MOD
+print(dp[K][3])
