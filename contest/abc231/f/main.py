2082ee6764106d2cc6cfc7a33d92cf9154bb87bd
iorn121 <wmt.tkn.121@gmail.com>
2024-02-13 00:21:07 +0900

    abc231 F

--- contest/abc231/f/main.py
+++ contest/abc231/f/main.py
@@ -1,29 +1,21 @@
+from sortedcontainers import SortedList
+from collections import deque
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-AB = []
-d = {}
-for i in range(N):
-    if A[i] not in d:
-        d[A[i]] = []
-    d[A[i]].append(B[i])
-    AB.append((A[i], B[i]))
-AB.sort()
-B_use = []
+AB=[[a,b] for a,b in zip(A,B)]
+AB.sort(key=lambda x: (x[0],-x[1]))
+dq = deque(AB)
+# print(dq)
 ans = 0
-for a in sorted(d):
-    for b in d[a]:
-        B_use.append(b)
-    B_use.sort()
-    for b in d[a]:
-        left = -1
-        right = len(B_use)
-        while right-left > 1:
-            mid = (left+right)//2
-            if B_use[mid] < b:
-                left = mid
-            else:
-                right = mid
-        ans += i-left
+S = SortedList([])
+while dq:
+    A,B = dq[0]
+    cnt = 0
+    while dq and dq[0] == [A,B]:
+        dq.popleft()
+        S.add(B)
+        cnt += 1
+    ans += cnt * (len(S) - S.bisect_left(B))
 
 print(ans)
