f50a5d12e4639bb3b80b66ad6f3f5c95ada1cbf3
iorn121 <wmt.tkn.121@gmail.com>
2024-02-17 02:15:59 +0900

    abc226 E

--- contest/abc226/e/main.py
+++ contest/abc226/e/main.py
@@ -1,5 +1,6 @@
 from atcoder.dsu import DSU
 N,M=map(int,input().split())
+G=[[] for _ in range(N)]
 if N!=M:
     print(0)
     exit()
@@ -7,14 +8,17 @@ uf=DSU(N)
 for _ in range(M):
     a,b=map(int,input().split())
     uf.merge(a-1,b-1)
+    G[a-1].append(b-1)
+    G[b-1].append(a-1)
 
-
-MOD=998244353
-ans=1
 for group in uf.groups():
-    if len(group)==1:
+    cnt=0
+    for v in group:
+        cnt+=len(G[v])
+    if cnt!=2*len(group):
         print(0)
         exit()
-    ans*=2
-    ans%=MOD
+
+MOD=998244353
+ans=pow(2,len(uf.groups()),MOD)
 print(ans)
